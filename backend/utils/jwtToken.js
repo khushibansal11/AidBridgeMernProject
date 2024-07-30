93a42a550264232c431e56292d37a029eff1dce6
khushibansal11 <khushibansal635@gmail.com>
2024-07-30 17:08:43 +0530

    updated to handel cookies

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,22 +1,29 @@
 // Create Token and saving in cookie
 const sendToken = (user, statusCode, res) => {
+  try {
     const token = user.getJWTToken();
-  
+    
     // options for cookie
     const options = {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      same_site: 'none',
-      secure: true
+      sameSite: 'none',
+      secure: process.env.NODE_ENV === 'PRODUCTION' ? true : false
     };
-  
+    
     res.status(statusCode).cookie("token", token, options).json({
       success: true,
       user,
       token,
     });
-  };
-  
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 module.exports = sendToken;
